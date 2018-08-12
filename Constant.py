e46d2280dd62706df7c3edfcfae2dbb14891cb0d
AdityaShete <32203937+AdityaShete@users.noreply.github.com>
2018-08-12 22:20:28 +0530

    Add files via upload

---
+++ Constant.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 12 03:58:09 2018
+
+@author: Jay
+"""
+
+#define constants
+lifespan = 30
+strenghtThreshold = 100 
+stdDevLife = 100
+gridRow = 1000
+gridCol = 1000
+nHumans = 10
+    
